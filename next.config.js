3ca671b2365aa0e0a8b12dc83c6b1ec7c1855558
rjoydip <joydipand@gmail.com>
2020-05-01 18:25:52 +0530

    optimize: removed unused css

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 const composePlugins = require('next-compose-plugins')
-const plugins = []
+const plugins = [
+  require('next-purgecss')({
+    purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer,
+    purgeCssPaths: [
+      './pages/**/*.{js,jsx,ts,tsx,css}',
+      './components/**/*.{js,jsx,ts,tsx,css}',
+      '*.{js,jsx,ts,tsx,css}',
+    ],
+  }),
+]
 const nextConfig = {}
 
 module.exports = composePlugins([...plugins], nextConfig)
